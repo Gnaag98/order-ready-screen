d812719e86d0ae7e42e4ffbf959163a52ef80b07
Henrik Wahlström <henrik.g.wahlstrom@pm.me>
2026-02-11 21:45:02 +0100

    Show orders as buttons in back
    The back view now shows orders as buttons, though they do nothing yet. Each view uses a separate template tag to enable different elements being added with the same logic.

--- static/common.js
+++ static/common.js
@@ -1,12 +1,14 @@
 /** Redraws visual of pending orders. */
 export function redrawPendingOrders(pendingOrders) {
 	const orders_element = document.getElementById('pending-orders');
+	const order_template = document.getElementById('pending-order');
 	// Clear list.
 	orders_element.textContent = '';
 	// Repopulate list.
 	for (let order of pendingOrders) {
-		const order_element = document.createElement('li');
-		order_element.textContent = order;
-		orders_element.appendChild(order_element);
+		const order_clone = document.importNode(order_template.content, true);
+		const order_number_element = order_clone.querySelector('.order');
+		order_number_element.textContent = order;
+		orders_element.appendChild(order_clone);
 	}
 }
